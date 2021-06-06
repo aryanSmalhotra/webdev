c4df0c76b167655c5db2e120d06d196a22eef5ff
aryanSmalhotra <malhotra.singh.aryan@gmail.com>
2021-06-06 12:42:00 +0530

    webdev

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 
 let age = prompt("enter your age")
-if (age > 18) {
-    console.log("You can use this site")
-} else if (age < 18) {
-    console.log("you can't use this site .")
-    if (Password.length >= 6) {
-
-    }
+if (age <= 5 || age >= 91) {
+    console.log("You get in for free");
+} else {
+    console.log("You pay $10")
+}
